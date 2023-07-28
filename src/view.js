e026de46922903847f69d41b5b3d0b30152544b7
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-07-28 10:04:26 -0400

    Add functionality to prevent tabbed browsing on sit when category form is visible

--- src/view.js
+++ src/view.js
@@ -1,13 +1,13 @@
-// export default function newCategoryForm() {
-//   const newCatForm = document.querySelector(".cat-form-container");
-//   const activeTabNav = document.querySelector("tab-btn--active");
-//   const activeTabContent = document.querySelector("tab-content--active");
-//   const categoryNav = document.querySelector(".category-tab");
-//   const categoryTab = document.querySelector(".by-category");
+export default function newCategoryForm() {
+  const newCatForm = document.querySelector(".cat-form-container");
+  const categoryTab = document.querySelector(".by-category");
 
-//   newCatForm.style.display = "initial";
-//   activeTabNav.classList.remove("tab-btn--active");
-//   activeTabContent.classList.remove("tab-content--active");
-//   categoryNav.classList.add("tab-btn--active");
-//   categoryTab.classList.add("tab-content--active");
-// }
+  newCatForm.style.display = "initial";
+  categoryTab.classList.add("tab-content--active");
+
+  if (newCatForm.style.display === "initial") {
+    const sideNav = document.querySelector(".side-nav");
+
+    sideNav.classList.add("cat-form-visible");
+  }
+}
